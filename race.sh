8e815c5392bbf728615940a7628db1bf3b9fbb63
sandi negara <angelitasalsabilaaf@gmail.com>
2024-03-31 15:16:57 +0700

    bismillah banget yu

--- race.sh
+++ race.sh
@@ -1,34 +1,18 @@
 #! /bin/bash
 echo "hello"
-i=1
-for y in $(seq 1 100)
-do
-    (for x in $(seq 1 100)
-    do
-        start_time=$(date +%s%N) # Waktu mulai dalam nanodetik
-        response=$(curl -s "http://${{ env.URL }}/login.php?id=1&amount=100")
-        end_time=$(date +%s%N) # Waktu selesai dalam nanodetik
-        elapsed_time=$((end_time - start_time)) # Waktu eksekusi dalam nanodetik
-        
-        echo "Response from curl: $response"
-        echo "Elapsed time: $((elapsed_time / 1000000)) ms" # Konversi ke milidetik
-    done) &
-done
-
-#! /bin/bash
-echo "hello"
-total_start_time=$(date +%s%N) # Waktu mulai keseluruhan
+total_start_time=$(date +%s) # Waktu mulai keseluruhan
 
 temp_file=$(mktemp) # Membuat file sementara untuk menyimpan waktu eksekusi
 
 for y in $(seq 1 100); do
     (
     for x in $(seq 1 100); do
-        start_time=$(date +%s%N) # Waktu mulai
+        start_time=$(date +%s) # Waktu mulai
         response=$(curl -s "http://${{ env.URL }}/login.php?id=1&amount=100")
-        end_time=$(date +%s%N) # Waktu selesai
+        end_time=$(date +%s) # Waktu selesai
         elapsed_time=$((end_time - start_time)) # Waktu eksekusi
-        echo $((elapsed_time / 1000000)) >> $temp_file # Menyimpan ke file sementara
+
+         echo $elapsed_time >> $temp_file # Menyimpan ke file sementara
     done
     ) &
 done
@@ -40,6 +24,10 @@ while read time; do
     total_elapsed_time=$((total_elapsed_time + time))
 done < $temp_file
 
-echo "Total Elapsed Time: $total_elapsed_time ms"
+# Menghitung total waktu dalam menit dan detik
+total_minutes=$((total_elapsed_time / 60))
+total_seconds=$((total_elapsed_time % 60))
+
+echo "Total Elapsed Time: ${total_minutes} minutes and ${total_seconds} seconds"
 
 rm $temp_file # Membersihkan file sementara
